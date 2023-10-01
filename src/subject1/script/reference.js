9c2ccff6617442edecf10d4fb50c5f17f245ed34
QnjfRbq <143937603+QnjfRbq@users.noreply.github.com>
2023-10-02 06:44:06 +0900

    2차 최종 제출 템플릿

--- src/subject1/script/reference.js
+++ src/subject1/script/reference.js
@@ -1,9 +1,23 @@
 const reference = [
   {
-    author: "",
-    title: "",
-    year: "",
-    sourceTitle: "",
+    author: "권도균",
+    title: "불교 업(業)사상의 현대적 의미",
+    year: "2008",
+    sourceTitle: "교정담론 제2권 제2호",
     link: "",
+  },
+  {
+    author: "Chaiya",
+    title: "Karma in Buddhism",
+    year: "1988",
+    sourceTitle: "at Suan Mokkhabalārāma",
+    link: "",
+  },
+  {
+    author: "백도수",
+    title: "업(Karma)명상 심리치료의 불교적 토대",
+    year: "2021",
+    sourceTitle: "동아시아불교문화 45집, 2021. 3, 303~330",
+    link: "http://dx.doi.org/10.21718/EABC.2021.45.11",
   },
 ];
--- src/subject3/script/reference.js
+++ src/subject3/script/reference.js
@@ -1,9 +1,9 @@
 const reference = [
   {
-    author: "",
-    title: "",
-    year: "",
-    sourceTitle: "",
-    link: "",
+    author: "Wolf Erlbruch",
+    title: "내가 함께 있을게",
+    year: "2007",
+    sourceTitle: "웅진주니어",
+    link: "https://search.shopping.naver.com/book/catalog/32438196628?cat_id=50006101&frm=PBOKPRO&query=%EB%82%B4%EA%B0%80+%ED%95%A8%EA%BB%98+%EC%9E%88%EC%9D%84%EA%B2%8C+%EB%8F%99%ED%99%94&NaPm=ct%3Dln7x2cn4%7Cci%3D81bd40329b0fd275ab72ee70024777d4b8489d3d%7Ctr%3Dboknx%7Csn%3D95694%7Chk%3D20635870e55e722c6103502f57de04374e2b94b0",
   },
 ];
